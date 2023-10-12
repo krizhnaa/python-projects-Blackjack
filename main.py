ac28ddb85615d172bc9847d184c9dc994eaeed36
KRISHNA PREM <86138069+krizhnaa@users.noreply.github.com>
2023-10-12 19:47:23 +0530

    Final changes

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+############### Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
 import random
 
 game = True
@@ -9,20 +21,56 @@ def drawcard():
 
 # dc = drawcard()
 
-ux = drawcard()
-uy = drawcard()
-uz = drawcard()
-utot = ux + uy
+usercardlist = []
+compcardlist = []
 
-cx = drawcard()
-cy = drawcard()
+usercardlist.append(drawcard())
+usercardlist.append(drawcard())
+usertot = 0
 
-print(f"Your cards are : [{ux}, {uy}], current score : {utot}")
-print(f"The Computer's First card is : [{cx}]")
+compcardlist.append(drawcard())
+compcardlist.append(drawcard())
+comptot = 0
 
-draw = input("Do you want to draw a card again?")
+for x in usercardlist:
+  usertot+=x
 
-utot = ux + uy + uz
+for y in compcardlist:
+  comptot+=y
 
-if draw == 'y':
-  print(f"Your cards are : [{ux}, {uy}, {uz}], current score : {utot}")
+print(f"Your cards are : {usercardlist}, curr score : {usertot}")
+print(f"Computer's First hand : {compcardlist[0]}")
+
+while comptot < 17:
+  y = drawcard()
+  compcardlist.append(y)
+  comptot+=y
+
+while True:
+  draw = input("Do you want to draw again? 'y' or 'n'  ")
+  if draw == 'y':
+    x = drawcard()
+    usercardlist.append(x)
+    usertot+=x
+    print(f"Your cards are : {usercardlist}, curr score : {usertot}")
+  else:
+    break;
+
+if usertot < 22 and comptot < 22:
+  if usertot > comptot:
+    print("The user wins")
+  else:
+    print("The Dealer wins")
+elif usertot > 21:
+  print("Dealer wins")
+elif comptot > 21:
+  print("User Wins")
+
+
+print(usercardlist, usertot)
+print(compcardlist, comptot)
+
+
+  
+  
+  
